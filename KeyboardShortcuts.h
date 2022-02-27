fc54d89877bf7c9a7373ec5d878cd012a9d04fb9
Garly Nugraha <98641173+garllockhart@users.noreply.github.com>
2022-02-27 14:22:36 +0700

    Make KeyboardShortcuts

---
+++ KeyboardShortcuts.h
@@ -0,0 +1,14 @@
+#ifndef KeyboardShortcuts_H
+#define KeyboardShortcuts_H
+
+#define COPY 3 /* CTRL + C */
+#define BACKSPACE 8
+#define TAB 9
+#define ENTER 13
+#define PASTE 22
+#define REDO 25
+#define UNDO 26
+#define SPACE 32
+#define DELETE 127
+
+#endif

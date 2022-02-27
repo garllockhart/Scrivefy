ed0e7cd6e7ece6431d3c39daa75276c2340de77b
Garly Nugraha <98641173+garllockhart@users.noreply.github.com>
2022-02-27 15:22:19 +0700

    Make Time Date

---
+++ TimeDate.cpp
@@ -0,0 +1,8 @@
+/*
+File Name		: TimeDate.cpp
+Description		: 
+Author			: 
+Date			: 27/02/2022
+*/
+
+
---
+++ TimeDate.h
@@ -0,0 +1,11 @@
+/*
+File Name		: TimeDate.h
+Description		: 
+Author			: 
+Date			: 27/02/2022
+*/
+
+#ifndef TimeDate_H
+#define TimeDate_H
+
+#endif

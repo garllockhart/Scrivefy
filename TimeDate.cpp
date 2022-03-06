a07dc8eff027b162d5213480da095ae9e108db51
Yumi Fee <95599683+yumifee@users.noreply.github.com>
2022-03-06 22:50:30 +0700

    Update TimeDate.cpp

--- TimeDate.cpp
+++ TimeDate.cpp
@@ -5,4 +5,24 @@ Author			:
 Date			: 27/02/2022
 */
 
+#include <stdio.h>
+#include <time.h>
+#define TIME 7
+const char* const HARI[7] = {"Minggu","Senin","Selasa","Rabu","Kamis","Jumat","Sabtu"};
+const char * const BULAN[12] = {"Januari","Februari","Maret","April","Mei","Juni","Juli","Agustus","September","Oktober","November","Desember"};
 
+
+int main(){
+	time_t t;
+	struct tm now;
+	
+	t = time (NULL);
+	now = *localtime(&t);
+	
+	if (7 == TIME){
+	printf ("%s,%d %s %d\n",HARI[now.tm_wday],now.tm_mday,BULAN [now.tm_mon],now.tm_year + 1900);
+	}
+	
+	
+	return 0;
+}

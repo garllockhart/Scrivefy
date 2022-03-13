109efc611d52a3fc6f2233b5f470e3334d41599d
jeremiaedwin <edwinjeremia00@gmail.com>
2022-03-13 20:11:10 +0700

    input text array 2d

--- inputText.cpp
+++ inputText.cpp
@@ -16,40 +16,72 @@ void inputText(text *newText, char file_name[]){
 	int i;
 	int j;
 	char temp;
-	for(i=0; i<ROWS; i++){
-		for(j=0; j<COLUMNS; j++){
+	for(i = 0; i<ROWS; i++){
+		baris = i;
+		for(;;){
 			temp = getch();
+			top++;
 			setText(&(*newText), temp, i, j);
 			if(newText->column[j] == SAVE){
 				saveFile(&(*newText), file_name);
+			if(top < 0){
+				break;
 			}
+			displayText(*newText);
 		}
+		
 	}
-	
-	displayText(*newText);
 }
 
 void setText(text *newText,char temp, int i, int j){
-	newText->rows[i] = i;
-	switch(temp){
-		case TAB : 
-			temp = TAB;
-			newText->column[j] = temp;
-			break;
-		case SPACE :
-			temp = SPACE;
-			newText->column[j] = temp;
-			break;
-		case SAVE :
-			temp = SAVE;
-			newText->column[j] = temp;
-			break;
-		default:
-			newText->column[j] = temp;
-			break;
-	}
+//	newText->rows[i] = i;
+//	switch(temp){
+//		case TAB : 
+//			temp = TAB;
+//			newText->column[j] = temp;
+//			break;
+//		case SPACE :
+//			temp = SPACE;
+//			newText->column[j] = temp;
+//			break;
+//		case SAVE :
+//			temp = SAVE;
+//			newText->column[j] = temp;
+//			break;
+//		default:
+//			newText->column[j] = temp;
+//			break;
+//	}
+//	
+//	printf("%c", newText->column[j]);
+text undoText;
 	
-	printf("%c", newText->column[j]);
+    if(top>=n-1)
+    {
+        top = 0;
+    	baris++;
+        
+    }
+    else
+    {
+    	switch(temp){
+    		case 8:
+    			backspace(*newText, &undoText);
+    			printf("\nini undo %c\n", undoText.column[baris][top]);
+    			break;
+    		case 13:
+    			newText->column[baris][top] = '\n';
+    			top = -1;
+    			baris++;
+				break;
+			case 26:
+				copyString(undoText, &(*newText));
+				break;
+    		default:
+    			newText->column[baris][top] = temp;
+    			break;
+		}	
+    }
 }
 
 void displayText(text newText){
